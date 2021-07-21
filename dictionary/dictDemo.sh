fc2ba3b9d31c0a8819a6b557162d2b995d41205e
Artimirche6 <artimirche6@gmail.com>
2021-07-21 19:05:49 +0530

    Added : Array and Dictionary

---
+++ dictionary/dictDemo.sh
@@ -0,0 +1,16 @@
+#!/bin/bash/ -x
+
+declare -A sounds
+
+
+sounds[dog]="bark"
+sounds[cow]="moo"
+sounds[bird]="tweet"
+sounds[wolf]="howl"
+
+echo "Dog sound - " ${sounds[dog]}
+echo "All animals sound - " ${sounds[@]}
+echo "All animals - " ${!sounds[@]}
+echo "Number of Animals - " ${#sounds[@]}
+unset sounds[bird]
+echo "After deleting all animals - " ${!sounds[*]}
